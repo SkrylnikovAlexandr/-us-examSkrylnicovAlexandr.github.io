e246e1eac14d35101f52a8fe954efa15db866f82
SkrylnikovAlexandr <alekosandr883@gmail.com>
2025-05-14 12:56:08 +0500

    Delete index-script1.js

--- index-script1.js
+++
@@ -1,1 +0,0 @@
-let likeCount=0,dislikeCount=0;function increaseLikes(){likeCount++,document.getElementById("like-text").textContent=`Вы нажали “Мне нравится сайт” ${likeCount} раз.`}function increaseDislikes(){dislikeCount++,document.getElementById("dislike-text").textContent=`Вы нажали “Мне не нравится сайт” ${dislikeCount} раз.`}
